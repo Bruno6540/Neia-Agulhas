ae6d3657ce2d9680ec32e56e9feb56f00e2c1e7c
Bruno6540 <brfernandes.silva@gmail.com>
2025-10-17 15:46:02 -0300

    titulos dos modais

--- produtos.js
+++ produtos.js
@@ -39,7 +39,7 @@ fetch("./produtos.json")
         <div class="modal-dialog modal-lg modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title">${produto.nome}</h5>
+              <h5 class="modal-title modal-titulo">${produto.nome}</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
             </div>
             <div class="modal-body">
@@ -78,7 +78,7 @@ fetch("./produtos.json")
               </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+              <button type="button" class="btn botao-claro btn-secondary" data-bs-dismiss="modal">Fechar</button>
             </div>
           </div>
         </div>
